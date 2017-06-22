37ef0174f8be5c7face6f72d5092c7c989e65025
Tim <tgerstel@umass.edu>
2017-06-22 00:15:47 -0400

    Made methods static

--- src/com/thetimg/aq/util/BiPolarUtil.java
+++ src/com/thetimg/aq/util/BiPolarUtil.java
@@ -1,15 +1,12 @@
 package com.thetimg.aq.util;
 
-/**
- * Created by Tim on 6/21/17.
- */
 public class BiPolarUtil {
     
-    public double toDouble(boolean value){
+    public static double toDouble(boolean value){
         return (value ? 1.0 : -1.0);
     }
     
-    public boolean toBipolar(double value){
+    public static boolean toBipolar(double value){
         return (value == 1.0);
     }
     
