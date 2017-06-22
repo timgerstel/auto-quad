bfd2369ba6683df80516bb2d469b85aee619db74
Tim <tgerstel@umass.edu>
2017-06-22 00:14:02 -0400

    Added methods: isVector, set, isEmpty, sum, rowSubmatrix, colSubmatrix, submatrix, toPackedArray.  Made createColumnMatrix and createRowMatrix static.  Fixed cloning constructor.

--- src/com/thetimg/aq/math/Matrix.java
+++ src/com/thetimg/aq/math/Matrix.java
@@ -4,14 +4,24 @@ public class Matrix {
     
     private double[][] matrix;
     private int rows, cols;
+    private boolean isVector = false;
+    protected boolean isEmpty = true;
     
     public Matrix(int rows, int cols){
         this.rows = rows;
         this.cols = cols;
+        if(rows == 1 || cols == 1){
+            isVector = true;
+        }
         this.matrix = new double[rows][cols];
     }
     
     private Matrix(double[][] matrix){
+        this.rows = matrix.length;
+        this.cols = matrix[0].length;
+        if(rows == 1 || cols == 1){
+            isVector = true;
+        }
         this.matrix = matrix;
     }
     
@@ -26,17 +36,18 @@ public class Matrix {
     }
     
     //Returns an empty column matrix
-    public Matrix createColumnMatrix(int cols){
+    public static Matrix createColumnMatrix(int cols){
        return new Matrix(1, cols);
     }
     
     //Returns an empty row matrix
-    public Matrix createRowMatrix(int rows){
+    public static Matrix createRowMatrix(int rows){
         return new Matrix(rows, 1);
     }
     
     //Add value to every element in matrix
     public void add(double value){
+        isEmpty = false;
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 matrix[i][j] = matrix[i][j] + value;
@@ -45,13 +56,16 @@ public class Matrix {
     }
     
     //Set every matrix value to 0
-    public void clear(double value){
+    public void clear(){
+        isEmpty = true;
         matrix = new double[rows][cols];
     }
     
     //Returns a copy of this Matrix object
     public Matrix clone(){
-        return new Matrix(matrix);
+        Matrix ret = new Matrix(matrix);
+        ret.isEmpty = isEmpty;
+        return ret;
     }
     
     //Return value at parameter indices
@@ -59,11 +73,87 @@ public class Matrix {
         return matrix[row][col];
     }
     
+    public void set(int row, int col, double value) {
+        isEmpty = false;
+        matrix[row][col] = value;
+    }
+    
     //Return true if the m is equal to this object
     public boolean equals(Matrix m){
         if(rows != m.getRows() || cols != m.getCols()){
             return false;
         }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(matrix[i][j] != m.get(i, j)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean isVector(){
+        return this.isVector;
+    }
+    
+    public boolean isEmpty(){
+        return isEmpty;
+    }
+    
+    public double sum(){
+        if(isEmpty()){
+            return 0;
+        }
+        double sum = 0;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                sum = sum + matrix[i][j];
+            }
+        }
+        return sum;
+    }
+    
+    public Matrix rowSubmatrix(int row){
+        Matrix ret = new Matrix(1, cols);
+        ret.isEmpty = isEmpty;
+        for(int i = 0; i < cols; i++){
+            ret.set(0, i, matrix[row][i]);
+        }
+        return ret;
+    }
+    
+    public Matrix colSubmatrix(int col){
+        Matrix ret = new Matrix(rows, 1);
+        ret.isEmpty = isEmpty;
+        for(int i = 0; i < rows; i++){
+            ret.set(i, 0, matrix[i][col]);
+        }
+        return ret;
+    }
+    
+    //Return submatrix for the given start and end indices
+    public Matrix submatrix(int rowStart, int rowEnd, int colStart, int colEnd){
+        Matrix ret = new Matrix(rowEnd - rowStart,colEnd - colStart);
+        ret.isEmpty = isEmpty;
+        for(int i = 0; i < ret.getRows(); i++){
+            for(int j = 0; j < ret.getCols(); j++){
+                ret.set(i, j, matrix[rowStart + i][colStart + j]);
+            }
+        }
+        return ret;
+    }
+    
+    public double[] toPackedArray(){
+        double[] ret = new double[rows * cols];
+        int count = 0;
+        for(double[] row : matrix){
+            for(double val : row){
+                ret[count] = val;
+                count++;
+            }
+        }
+        return ret;
     }
     
 }
