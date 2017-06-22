cc4eb158eb5f99875df96c4b8358d1e0508ee54e
Tim <tgerstel@umass.edu>
2017-06-22 00:16:01 -0400

    Testing for BiPolarUtil.java

---
+++ test/util/BiPolarUtilTest.java
@@ -0,0 +1,30 @@
+package util;
+
+import com.thetimg.aq.util.BiPolarUtil;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Tim on 6/22/2017.
+ */
+public class BiPolarUtilTest {
+    @Rule
+    public Timeout global = new Timeout(1000L, TimeUnit.MILLISECONDS);
+    
+    @Test
+    public void testToDouble(){
+        assertEquals(1.0, BiPolarUtil.toDouble(true), 0.0);
+        assertEquals(-1.0, BiPolarUtil.toDouble(false), 0.0);
+    }
+    
+    @Test
+    public void testToBipolar(){
+        assertTrue(BiPolarUtil.toBipolar(1.0));
+        assertFalse(BiPolarUtil.toBipolar(-1.0));
+    }
+}
