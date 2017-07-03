941ac8aac0d50c27609300641a055fa496e857fe
Tim <tgerstel@umass.edu>
2017-07-02 23:06:03 -0400

    Added 2d array conversion.

--- src/com/thetimg/aq/util/BiPolarUtil.java
+++ src/com/thetimg/aq/util/BiPolarUtil.java
@@ -6,8 +6,28 @@ public class BiPolarUtil {
         return (value ? 1.0 : -1.0);
     }
     
+    public static double[][] toDouble(boolean[][] data){
+        double[][] ret = new double[data.length][data[0].length];
+        for(int i = 0; i < ret.length; i++){
+            for(int j = 0; j < ret[0].length; j++){
+                ret[i][j] = toDouble(data[i][j]);
+            }
+        }
+        return ret;
+    }
+    
     public static boolean toBipolar(double value){
         return (value == 1.0);
+    }
+    
+    public static boolean[][] toBipolar(double[][] data){
+        boolean[][] ret = new boolean[data.length][data[0].length];
+        for(int i = 0; i < ret.length; i++){
+            for(int j = 0; j < ret[0].length; j++){
+                ret[i][j] = toBipolar(data[i][j]);
+            }
+        }
+        return ret;
     }
     
 }
